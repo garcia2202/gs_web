405f1226b4e302241676a88949231066ce03deb4
garcia2202 <tonato.thiago@gmail.com>
2023-11-15 17:03:25 -0300

    feito footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,24 @@
 import {} from "react";
+import "../css/Footer/footer.css";
+import { FaFacebook } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa6";
 
 function Footer() {
   return (
-      <footer>
-        <p>Footer</p>
-      </footer>
+    <footer>
+      <p>Todos os direitos reservados.</p>
+      <p>2023</p>
+      <div className="redes_div">
+        <FaFacebook className="redes" />
+        <FaGithub className="redes" />
+        <FaInstagram className="redes" />
+        <FaTwitter className="redes" />
+        <FaYoutube className="redes" />
+      </div>
+    </footer>
   );
 }
 
