8a1517f1d60498f31fe0908b5f72c0d3f89207a4
garcia2202 <tonato.thiago@gmail.com>
2023-11-22 12:55:54 -0300

    arrumado router para o main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import {} from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./components/Login.jsx";
-import Home from "./components/Home.jsx";
+import { Outlet } from "react-router-dom";
 import Nav from "./components/Nav.jsx";
 import Footer from "./components/Footer.jsx";
 
@@ -9,12 +7,7 @@ function App() {
   return (
     <>
       <Nav />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </Router>
+      <Outlet />
       <Footer />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import GlobalStyle from './css/Global.jsx'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Home from "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import Error from "./components/Error.jsx";
+import GlobalStyle from "./css/Global.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+
+    children: [
+      { path: "/", element: <Login /> },
+      { path: "/home", element: <Home /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <RouterProvider router={router} />
     <GlobalStyle />
-  </React.StrictMode>,
-)
-
+  </>
+);
